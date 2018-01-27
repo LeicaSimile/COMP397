b3768c7fddab8cacc439a682d2b789911afd38d6
LeicaSimile <leicasimile@gmail.com>
2018-01-27 15:21:37 -0500

    Add typescript support

--- lesson01/Scripts/core/index.js
+++ lesson01/Scripts/core/index.js
@@ -1,29 +1,23 @@
 // IIFE - Immediately Invoked Function Expression
-(function(){
-
-    let canvas = document.getElementById("canvas");
-    let stage: createjs.Stage;
-
-    function Init():void {
+(function () {
+    var canvas = document.getElementById("canvas");
+    var stage;
+    function Init() {
         console.log("Game Loaded!");
         Start();
     }
-
-    function Start():void {
+    function Start() {
         stage = new createjs.Stage(canvas);
         createjs.Ticker.framerate = 60; // 60 fps
         createjs.Ticker.on("tick", Update); // calls every frame
         Main();
     }
-
-    function Update():void {
+    function Update() {
         stage.update();
     }
-
-    function Main():void {
+    function Main() {
         console.log("Starting game...");
     }
-
     window.onload = Init;
-
 })();
+//# sourceMappingURL=index.js.map
