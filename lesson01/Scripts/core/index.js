192ca2284c13dd117dffd9e6956071a27c08b48d
LeicaSimile <leicasimile@gmail.com>
2018-01-27 14:36:29 -0500

    Add IIFE for init functions

---
+++ lesson01/Scripts/core/index.js
@@ -0,0 +1,22 @@
+// IIFE - Immediately Invoked Function Expression
+(function(){
+
+    function Init() {
+        console.log("Game Loaded!");
+    }
+
+    function Start() {
+
+    }
+
+    function Update() {
+
+    }
+
+    function Main() {
+
+    }
+
+    window.onload = Init;
+
+})();
